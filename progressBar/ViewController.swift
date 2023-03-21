0457675de0005fe506c70b7a3e61dc707663144e
janvi kachchhi <janukachchhi2@gmail.com>
2023-03-21 22:15:44 +0530

    .....

--- progressBar/ViewController.swift
+++ progressBar/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressbar: UIProgressView!
+    
+    @IBOutlet weak var playButton: UIButton!
+    var time = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        progressbar.progress = 0.0
 
+    }
+    
+    @IBAction func playButtonaction(_ sender: UIButton) {
+        
+        
+            var a : Float = 0.0
+            self.progressbar.progress = a
+            time = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { (time) in
+                a += 0.01
+                self.progressbar.progress = a
+                if self.progressbar.progress == 1.0
+                {
+           
+                    self.time.invalidate()
+                    self.naviget()                }
+            })
+    }
+    func naviget()
+    {
+        let navigation = storyboard?.instantiateViewController(withIdentifier: "ViewController2")as! ViewController2; navigationController?.pushViewController(navigation, animated: true)
+    }
+    
 
 }
 
---
+++ progressBar/ViewController2.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController2.swift
+//  progressBar
+//
+//  Created by R94 on 21/03/23.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
